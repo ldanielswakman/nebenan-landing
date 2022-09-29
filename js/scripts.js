13d7efc430378b7d3f910291a4cb8435006a51ac
L Daniel Swakman <d.swakman@gmail.com>
2022-09-29 17:52:17 +0200

    Added inter & header tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,13 @@ function headerOnScroll($obj) {
         $obj.css('top', 0);
         $obj.addClass('is-sticky');
     }
+
+    colourThreshold = $('.section--cover').offset().top + $('.section--cover').outerHeight() - $('header').outerHeight();
+    if(scroll > colourThreshold) {
+        $('header').addClass('isWhite');
+    } else {
+        $('header').removeClass('isWhite');
+    }
 }
 
 function startProgressBar() {
